6930fe69b2c37a2148ec20203fef3ed39f7d5e4e
om-g4rg <omgarg3000@gmail.com>
2025-01-13 09:30:40 +0530

    calculator page is done

--- calculator.js
+++ calculator.js
@@ -1,41 +1,59 @@
 'use strict'
 
+const popup=document.querySelector(`.back`);
 const inputBtn=document.querySelectorAll(`.btn`);
+const opBtn=document.querySelectorAll(`.char`);
 const inputBox=document.querySelector(`.input`);
 const outBox= document.querySelector(`.output`);
 const clearBtn=document.querySelector(`#clear`);
 const deleteBtn=document.querySelector(`#delete`);
 const addBtn=document.querySelector(`#add`);
 const ansBtn=document.querySelector(`#equal`);
+const saveBtn=document.querySelector(`#save`);
+const closeBtn= document.querySelector(`.close`);
+const storeRes= document.querySelector(`.result`);
 
+inputBox.textContent=``;
+outBox.textContent=``;
 
-let globalVal=0;
 
 inputBtn.forEach(element => {
     element.addEventListener(`click`, function(){
         inputBox.textContent=`${inputBox.textContent}${element.textContent}`;
+       })
+});
+
+
+opBtn.forEach(element => {
+    element.addEventListener(`click`, function(){
+        if(inputBox.textContent!=`` & inputBox.textContent.charAt(inputBox.textContent.length-1)!=`+` & inputBox.textContent.charAt(inputBox.textContent.length-1)!=`-`& inputBox.textContent.charAt(inputBox.textContent.length-1)!=`*`&inputBox.textContent.charAt(inputBox.textContent.length-1)!=`/`){
+        inputBox.textContent=`${inputBox.textContent}${element.textContent}`;
+        }
     })
 });
+
+
 clearBtn.addEventListener(`click`, function(){
     inputBox.textContent=``;
+    outBox.textContent=``;
 })
 deleteBtn.addEventListener(`click`, function(){
     inputBox.textContent= inputBox.textContent.slice(0,-1);
 })
+
 ansBtn.addEventListener(`click`, function(){
-    globalVal+= parseFloat(inputBox.textContent);
-    console.log(globalVal);
-    outBox.textContent=`${outBox.textContent}${inputBox.textContent}= ${globalVal}`;
-    inputBox.textContent=``;
+    const str=inputBox.textContent;
+    outBox.textContent=`= ${eval(str)}`;
 })
 
-
+closeBtn.addEventListener(`click`, function(){
+    popup.classList.add(`hidden`);
+})
 //-------------------------------------------
-addBtn.addEventListener(`click`, function(){
-    if(inputBox.textContent!=``){
-        globalVal+=parseFloat(inputBox.textContent);
-        console.log(globalVal);
-        outBox.textContent=`${outBox.textContent}${inputBox.textContent}+`;
-        inputBox.textContent=``;
+
+saveBtn.addEventListener(`click`, function(){
+    if(popup.classList.contains(`hidden`)){
+        popup.classList.remove(`hidden`);
+        storeRes.textContent=outBox.textContent;
     }
 })
